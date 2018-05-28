0a29f6e2e67be044f065936f4720bd6023339891
Scottie Schneider <scott@laclave.co>
2018-05-28 15:04:29 -0500

    hello world

---
+++ routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  let response = {
+    formId: req.body.formId,
+    email: req.body.email
+  };
+  console.log(response.email);
+  axios({
+    method: 'post',
+    url: 'https://api.fullcontact.com/v3/person.enrich',
+    headers: { 'Authorization': 'Bearer oQllYgg7z1JMzqH62kqg3jI0s4ofb9Ev' },
+    data: {"email": "scott@warcat.co"}
+})
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+})
+
+module.exports = router;
