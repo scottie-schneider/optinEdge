1fdc5f9ddba329d363a2871618b14180d327e147
Scottie Schneider <scott@laclave.co>
2018-05-28 19:57:12 -0500

    cors

--- client_side.js
+++ client_side.js
@@ -1,6 +1,6 @@
 let email = "2e00f211473c1a6c828a9ec22ba5950b-0";
 let fullname = "2e00f211473c1a6c828a9ec22ba5950b-1";
-var cheerio = require('cheerio');
+
 
 //document.getElementById(email).addEventListener("change", change);
 
@@ -26,27 +26,3 @@ function change () {
     }
   })
 }
-
-function now () {
-  cheerio.ajax({
-    url: 'https://frozen-dawn-92571.herokuapp.com/form',
-    dataType: 'json',
-    type: 'post',
-    contentType: 'application/json',
-    data:
-      {
-        //"email": "scott.schneider09@yahoo.com"
-        "email": "scott@warcat.co",
-        "formId": "1234"
-      },
-    processData: false,
-    success: function( data, textStatus, jQxhr ){
-      console.log(data)
-        document.getElementById(fullname).value = data.fullName;
-    },
-    error: function( jqXhr, textStatus, errorThrown ){
-        console.log( errorThrown );
-    }
-  })
-}
-now();
