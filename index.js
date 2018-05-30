eb85e536c9d40b7b1b4aad4ce15a3983423c22d4
Scottie Schneider <scott@laclave.co>
2018-05-30 17:01:10 -0500

    all cors

--- index.js
+++ index.js
@@ -24,12 +24,46 @@ const routes = require('./routes');
 
 app.use(jsonParser());
 
-app.use('/form', routes);
+//app.use('/form', routes);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'index.html'));
 });
 
+app.post('/', (req, res) => {
+
+  let response = {
+    formId: req.body.formId,
+    email: req.body.email
+  };
+  console.log(response.email);
+  axios({
+    method: 'post',
+    url: 'https://api.fullcontact.com/v3/person.enrich',
+    headers: { 'Authorization': 'Bearer oQllYgg7z1JMzqH62kqg3jI0s4ofb9Ev' },
+    data: {"email": `${response.email}`}
+  })
+  .then((response) => {
+    res.send(response.data);
+    return axios({
+      method: 'post',
+      url: 'https://copyblondie.bubbleapps.io/version-test/api/1.1/wf/test/',
+      headers: { 'Authorization': 'Bearer 98107ac3b7b363d93f1b9e3863b79bee' },
+      data: response.data
+    })
+    .then((response) => {
+      console.log('hi')
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+
+
+})
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 })
