890ebfea77a3800990dd136aa8b73c6cb2762176
Scottie Schneider <scott@laclave.co>
2018-05-30 18:05:53 -0500

    all cors

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const axios = require('axios');
 const jsonParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
-app.use('*', cors());
-app.options('/form', cors())
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,HEAD,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
@@ -36,7 +33,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'index.html'));
 });
 
-app.post('/form', cors(), (req, res) => {
+app.post('/form', (req, res) => {
 
   let response = {
     formId: req.body.formId,
