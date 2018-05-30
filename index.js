ff1c07a2d8533cd69e6e2efb0aba548d48070dae
Scottie Schneider <scott@laclave.co>
2018-05-30 17:45:39 -0500

    all cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const axios = require('axios');
 const jsonParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
