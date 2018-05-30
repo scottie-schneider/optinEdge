9fdf753290fe6236dc85edb0bf8f9e20def0ce13
Scottie Schneider <scott@laclave.co>
2018-05-30 16:59:11 -0500

    all cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   next();
 });
 
