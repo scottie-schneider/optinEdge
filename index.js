4cc92062d1c9617868d184d60410ac5692ef266d
Scottie Schneider <scott@laclave.co>
2018-05-30 18:22:11 -0500

    all cors

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/form', (req, res) => {
   axios({
     method: 'post',
     url: 'https://api.fullcontact.com/v3/person.enrich',
-    headers: { 'Authorization': 'Bearer oQllYgg7z1JMzqH62kqg3jI0s4ofb9Ev' },
+    headers: { 'Authorization': 'Bearer r253Ftnu1NhEhcgvmASo1qcWrfcNptPf' },
     data: {"email": `${response.email}`}
   })
   .then((response) => {
