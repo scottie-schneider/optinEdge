2e0d6d4887bba07b1a39f5c2dc23f332a8ea7c78
Scottie Schneider <scott@laclave.co>
2018-05-30 17:17:32 -0500

    all cors

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const jsonParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", '*');
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
